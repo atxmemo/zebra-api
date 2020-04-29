3f0f0d6f159fd36936d8c6e35dca4b8a783f0329
Memo <memo@myhouseby.com>
2020-04-28 21:53:48 -0500

    Initialize project

---
+++ db/migrate/20200429024035_create_shots.rb
@@ -0,0 +1,10 @@
+class CreateShots < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shots do |t|
+      t.references :frame, null: false, foreign_key: true
+      t.integer :value
+
+      t.timestamps
+    end
+  end
+end
