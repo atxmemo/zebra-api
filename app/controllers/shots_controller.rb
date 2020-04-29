22660dd696fcfcfdfe6ab795eb8f8b92ead055be
Memo <memo@myhouseby.com>
2020-04-29 17:56:35 -0500

    Added logic to calculate shot logic

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -3,14 +3,46 @@ class ShotsController < ApplicationController
   def create
     @game = Game.find_by(id: params[:id])
 
-    @shot = Shot.new(value: params[:knocked_pins])
-    if @shot.save
+    if @game.completed?
+      render error: { error: 'This game has already been completed'}, status: 400
+    end
 
-      # TODO: calculate next available frame and add shot to that frame
+    current_frame = @game.current_frame
+    @shot = Shot.new(value: params[:knocked_pins], frame: current_frame)
+
+    if @shot.save
+      # First shot in frame
+      if current_frame.shots.size == 1
+        if @shot.value == 10
+          current_frame.update(status: :strike)
+        end
+
+      # Second shot in frame
+      elsif current_frame.shots.size == 2
+        score = @shot.value + current_frame.shots.first.value
+
+        if score > 10
+          render json: { error: 'Invalid shot value'}, status: 400
+        end
+
+        # Spare frames do not have value until next shot is decided
+        if score == 10
+          current_frame.update(status: :spare)
+        end
+
+        # Open frame scores are taken at face value
+        if score < 10
+          current_frame.update(status: :open, score: score)
+        end
+
+      # Third shot in frame (should only happen on tenth frame)
+      else
+
+      end
 
       render json: @shot
     else
-      render error: { error: 'Unable to create shot.'}, status: 400
+      render error: { error: "Unable to create shot: #{@shot.errors.messages}"}, status: 400
     end
   end
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,13 @@ class Game < ApplicationRecord
   has_many :frames
 
   enum status: { active: 0, completed: 1 }
-  
+
+  def current_frame
+    if frames.empty? || (!frames.last.available? && !frames.last.tenth_frame?)
+      frames << Frame.new
+    end
+
+    frames.last
+  end
+
 end
